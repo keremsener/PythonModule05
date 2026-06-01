950e93df9c0e04da8a7272cc2b1af3ed194ebcbd
ksener <blogkeremsener@gmail.com>
2026-06-01 15:20:13 +0300

    Implement DataProcessor, NumericProcessor and TextProcessor

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -1,11 +1,11 @@
 import abc
 import typing
 
-
 class DataProcessor(abc.ABC):
     def __init__(self) -> None:
-        pass
-
+        self.storage: list[tuple[int, str]] = []
+        self.counter: int = 0
+    
     @abc.abstractmethod
     def validate(self, data: typing.Any) -> bool:
         pass
@@ -15,4 +15,54 @@ class DataProcessor(abc.ABC):
         pass
 
     def output(self) -> tuple[int, str]:
-        pass
+        if not self.storage:
+            raise IndexError("Processor storage is empty.")
+        
+        return self.storage.pop(0)
+
+class NumericProcessor(DataProcessor):
+
+    def validate(self, data: typing.Any) -> bool:
+        if type(data) in (int, float):
+            return True
+
+        if type(data) is list:
+            if len(data) == 0:
+                return False
+            return all(type(item) in (int, float) for item in data)
+        return False
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if not self.validate(data):
+            raise ValueError("Improper numeric data")
+        if type(data) is list:
+            for item in data:
+                self.storage.append((self.counter, str(item)))
+                self.counter += 1
+        else:
+            self.storage.append((self.counter, str(data)))
+            self.counter += 1
+
+class TextProcessor(DataProcessor):
+
+    def validate(self, data: typing.Any) -> bool:
+        if type(data) is str:
+            return True
+
+        if type(data) is list:
+            if len(data) == 0:
+                return False
+            return all(type(item) is str for item in data)
+            
+        return False
+    
+    def ingest(self, data: str | list[str]) -> None:
+        if not self.validate(data):
+            raise ValueError("Improper text data")
+            
+        if type(data) is list:
+            for item in data:
+                self.storage.append((self.counter, item))
+                self.counter += 1
+        else:
+            self.storage.append((self.counter, data))
+            self.counter += 1
